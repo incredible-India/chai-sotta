4c7f856f4f4471a4f60f36e72b082c2e88e8e377
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-01-17 12:31:42 +0530

    added all the input box for thr persons

--- javascript/cal.js
+++ javascript/cal.js
@@ -14,12 +14,38 @@ NoPerButton.addEventListener('click', () => {
      for (let i = 0; i < nbrPersn; i++) {
      
          crtInput.innerHTML += `          <input type="text" class="form-control" value ="${i+1} person" > 
-         <input type="number" class="form-control" placeholder="tea" id='${1+1}stea' >
-         <input type="number" class="form-control" placeholder="sotta" id='${1+1}ssotta'>
-         <input type="number" class="form-control" placeholder="other" id='${1+1}sother'>`
-     }
+         <div class="input-group mb-3">
+         <span class="input-group-text">5.00</span>
+         <span class="input-group-text">x</span>
+  <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" placeholder="Tea" id="${i+1}c">
+</div>
+<div class="input-group mb-3">
+<span class="input-group-text">20.00</span>
+  <span class="input-group-text">x</span>
+
+  <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" placeholder="Sotta" id="${i+1}s">
+</div>
+        
+       
+<div class="input-group mb-3">
+
+
+  <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" placeholder="other" id="${i+1}o">
+</div>
+<hr>
+`
+     }//for loop excution end here
+
+    if(nbrPersn > 0)
+    {
+        crtInput.innerHTML += '    <button class="btn btn-warning" style="margin-top: 10px;">Calculate</button>'
+    }
+    
+
 
 
 })
+
+
 
 //now we will create number of input box as per persons
